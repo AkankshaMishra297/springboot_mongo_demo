c801aa7da927b6b13622e49fd8c00814fdd1f909
Akanksha <Akanksha@DESKTOP-JAR35DA>
2020-06-29 11:36:51 +0530

    created model, repo, service

---
+++ mongoDemo/src/main/java/com/neo/mongoDemo/model/User.java
@@ -0,0 +1,68 @@
+package com.neo.mongoDemo.model;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+	
+	@Id ObjectId id;
+
+	private String username;
+
+	private String password;
+
+	private boolean active;
+
+	private String createdAt;
+
+	
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", active=" + active
+				+ ", createdAt=" + createdAt + "]";
+	}
+
+
+}
